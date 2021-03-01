89b0f19f9f14a0882af01147e0103a8efa84fce3
Mariam Anuashvili <m.anuashvili@gmail.com>
2021-03-01 18:45:02 -0500

    my sol

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -1,13 +1,24 @@
-// --- Directions
-// Given an integer, return an integer that is the reverse
-// ordering of numbers.
-// --- Examples
-//   reverseInt(15) === 51
-//   reverseInt(981) === 189
-//   reverseInt(500) === 5
-//   reverseInt(-15) === -51
-//   reverseInt(-90) === -9
+// Given an int, return an int that is the reverse ordering of #'s
+// reverseInt(15) === 51
+// reverseInt(-90) === -9
+// reverseInt(500) === 5
 
-function reverseInt(n) {}
+function reverseInt(n) {
+    let str = n.toString()
+    let reversed = str.split("").reverse().join("")
+    let parsed = parseInt(reversed)
+    return Math.sign(n) * parsed
+}
+// reverseInt(-220)
+// Explanation:
+// .toString():
+// .parseInt():
+// Math.sign:
+
 
 module.exports = reverseInt;
+
+// str = n.toString()
+// reversed = str.split("").reverse().join("")
+// parsed = parseInt(reversed)
+// return Math.sign(n) * parsed
