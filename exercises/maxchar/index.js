fc9c009ccd1cbfdd4da5077e896098a749410ec5
Mariam Anuashvili <m.anuashvili@gmail.com>
2021-03-07 23:47:51 -0500

    working

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -27,12 +27,5 @@ function maxChar(str) {
     console.log(charMap)
 }
 
-/* Explanation:
-1) Create a new obj
-2) Iterate through the str
-3) For every char, add the char to the new obj
-4) Either assign it a value of 1 or if it already exists, increment the value by 1
-5) Several ways to add the chars to the obj: 1) split the str into an arr & use .forEach to loop over all the chars, 2) for of loop (like above)
-*/ 
 
 module.exports = maxChar;
