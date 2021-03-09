237edca6ac7b623616b88e5323489f7679fa60af
Mariam Anuashvili <m.anuashvili@gmail.com>
2021-03-09 17:54:13 -0500

    working

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -9,30 +9,42 @@ function maxChar(str) {
     charsArr.forEach(char => {
         charMap[char] = charMap[char]+1 || 1
     })
-
-    // for (let char of str) {
-        // works:
-        // if (charMap[char]) {
-        //     charMap[char]++
-        // } else {
-        //     charMap[char]=1
-        // }
-
-        // works:
-        // charMap[char] = charMap[char]+1 || 1 // why not ++ instead of charMap[char]+1
-
-        // ternary:
-        // charMap[char] ? charMap[char]++ : charMap[char]=1
-    // }
-    console.log(charMap)
+    // debugger
+    return Object.keys(charMap).reduce((a,b) => charMap[a] > charMap[b] ? a : b)
 }
+// maxChar("Hey There!")
 
 /* Explanation:
 1) Create a new obj
 2) Iterate through the str
 3) For every char, add the char to the new obj
 4) Either assign it a value of 1 or if it already exists, increment the value by 1
-5) Several ways to add the chars to the obj: 1) split the str into an arr & use .forEach to loop over all the chars, 2) for of loop (like above)
+5) Several ways to add the chars to the obj: 
+    1) .forEach
+    let charsArr = str.split("")
+    
+    charsArr.forEach(char => {
+        charMap[char] = charMap[char]+1 || 1
+    })
+
+    2) if...else w/in for...of
+    for (let char of str) {
+        if (charMap[char]) {
+            charMap[char]++
+        } else {
+            charMap[char]=1
+        }
+    }
+
+    3) Conditional w/in for...of
+    for (let char of str) {
+        charMap[char] = charMap[char]+1 || 1 // why not ++ instead of charMap[char]+1?
+    }
+
+    4) Ternary w/in for...of
+    for (let char of str) {
+        charMap[char] ? charMap[char]++ : charMap[char]=1
+    }
 */ 
 
 module.exports = maxChar;
