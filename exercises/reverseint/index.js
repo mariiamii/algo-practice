d9986a5061351d9334800fc1781fa6311e5fd29e
Mariam Anuashvili <m.anuashvili@gmail.com>
2021-03-09 18:12:50 -0500

    done

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -7,9 +7,11 @@ function reverseInt(n) {
     let reversed = n.toString().str.split("").reverse().join("")
     return Math.sign(n) * parseInt(reversed)
 }
-// Explanation:
-// .toString(): returns a str representing the object
-// .parseInt(): parses a str arg & returns an int
-// Math.sign(): returns either +/- 1, indicating the sign of a # passed into the arg
+
+/* Explanation:
+.toString(): returns a str representing the object
+.parseInt(): parses a str arg & returns an int
+Math.sign(): returns either +/- 1, indicating the sign of a # passed into the arg
+*/
 
 module.exports = reverseInt;
