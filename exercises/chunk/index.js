2573b03bf4920c898709c534b188802d42d52559
Mariam Anuashvili <m.anuashvili@gmail.com>
2021-03-10 22:52:48 -0500

    working

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -1,13 +1,13 @@
-// --- Directions
-// Given an array and chunk size, divide the array into many subarrays
-// where each subarray is of length size
-// --- Examples
+// Given an arr & chunk size, divide the arr into many subarrays, where each subarray is of length size
+// In the below eg, 2 is the # of elements that go into each chunk, not the total # of chunks
 // chunk([1, 2, 3, 4], 2) --> [[ 1, 2], [3, 4]]
 // chunk([1, 2, 3, 4, 5], 2) --> [[ 1, 2], [3, 4], [5]]
 // chunk([1, 2, 3, 4, 5, 6, 7, 8], 3) --> [[ 1, 2, 3], [4, 5, 6], [7, 8]]
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+    
+}
 
 module.exports = chunk;
