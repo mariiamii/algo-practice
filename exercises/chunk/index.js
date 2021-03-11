a6d18ee7dd45eb46c0aeed46c56cddccb15b914b
Mariam Anuashvili <m.anuashvili@gmail.com>
2021-03-11 16:45:57 -0500

    done

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -7,33 +7,21 @@
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
 // SOLUTION 1:
-// function chunk(array, size) {
-//     let chunked = []
-    
-//     array.forEach(ele => {
-//         let lastChunk = chunked[chunked.length - 1]
-
-//         if (!lastChunk || lastChunk.length === size) {
-//             chunked.push([ele])
-//         } else {
-//             lastChunk.push(ele)
-//         }
-//     })
-
-//     return chunked
-// }
-
-// SOLUTION 2:
 function chunk(array, size) {
     let chunked = []
-    let i = 0
+    
+    array.forEach(ele => {
+        let lastChunk = chunked[chunked.length - 1]
 
-    while (i < array.length) {
-        chunked.push(array.slice(i, i += size)) //.slice(0,2)
-    }
+        if (!lastChunk || lastChunk.length === size) {
+            chunked.push([ele])
+        } else {
+            lastChunk.push(ele)
+        }
+    })
 
     return chunked
-} 
+}
 
 /* Explanation:
 1) Create an empty arr to hold the "chuncked" chunks
@@ -41,6 +29,27 @@ function chunk(array, size) {
     1) Retrieve the last ele in "chunked"
     2) If the last ele does not exist, or if the last chunk's (sub-arr) length is equal to the chunk size, push a new chunk (sub-arr) into "chunked" w/ the current ele that you're iterating over
     3) Else, add the current ele into the chunk
+*/
+
+// SOLUTION 2:
+function chunk(array, size) {
+    let chunked = []
+    let i = 0
+
+    while (i < array.length) {
+        chunked.push(array.slice(i, i += size))
+    }
+
+    return chunked
+}
+
+/* Explanation:
+1) Create an empty arr to hold the "chuncked" chunks
+2) Set "index" variable equal to 0
+3) While index is less than array.length (iteration):
+    1) .slice(startIndex, endIndex): returns a slice of the array (1st arg includes ele, 2nd arg does not include ele);makes a copy, not destroys original array. E.g. array.slice(0,2)
+    2) Need to add "size" to "index" (i += size). E.g. array.slice(2,4)
+    1) Push the slice into "chunked"
 */
 
 module.exports = chunk;
