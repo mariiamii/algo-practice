52a1c1648aa544c9c7fc0c3406f19658df72bafa
Mariam Anuashvili <m.anuashvili@gmail.com>
2021-03-10 23:35:30 -0500

    done

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -7,7 +7,27 @@
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
 function chunk(array, size) {
+    let chunked = []
     
+    array.forEach(ele => {
+        let lastEle = chunked[chunked.length - 1]
+
+        if (!lastEle || lastEle.length === size) {
+            chunked.push([ele])
+        } else {
+            lastEle.push(ele)
+        }
+    })
+    
+    return chunked
 }
+
+/* Explanation:
+1) Create an empty arr to hold the "chuncked" chunks
+2) For each ele in the "unchunked" arr (iteration):
+    1) Retrieve the last ele in "chunked"
+    2) If the last ele does not exist, or if the chunk's (sub-arr) length is equal to the chunk size, push a new chunk (sub-arr) into "chunked" w/ the current ele that you're iterating over
+    3) Else, add the current ele into the chunk
+*/
 
 module.exports = chunk;
