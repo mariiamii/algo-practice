8d364ac4781cd243c03c96914342b2ff4623d747
Mariam Anuashvili <m.anuashvili@gmail.com>
2021-03-12 20:27:05 -0500

    working

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,7 +8,37 @@ anagrams("Hi there", "Bye there") --> False
 */
 
 function anagrams(stringA, stringB) {
-    
+    let cleanerA = stringA.replace(/[^\w]/g, "").toLowerCase()
+    let cleanerB = stringB.replace(/[^\w]/g, "").toLowerCase()
+
+    let arrA = cleanerA.split("")
+    let arrB = cleanerB.split("")
+
+    let objA = {}
+    let objB = {}
+
+    arrA.forEach(char => {
+        objA[char] = objA[char] + 1 || 1
+    })
+
+    arrB.forEach(char => {
+        objB[char] = objB[char] + 1 || 1
+    })
+
+    if (arrA.length != arrB.length) {
+        return false
+    } else if (Object.keys(objA) === Object.keys(objB) && Object.values(objA) === Object.values(objB)) {
+        return true
+    } else {
+        return false
+    }
 }
 
 module.exports = anagrams;
+
+/* Explanation:
+str = "hey there!"
+str.replace(/\s/g, " ") //=> "heythere"; \s is the regex for "whitespace", and g is the "global" flag, meaning match ALL \s (whitespaces)
+
+
+*/
