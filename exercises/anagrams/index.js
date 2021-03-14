0ce9ed518ba15a664e1b0d8406c4fb515c0858d6
Mariam Anuashvili <m.anuashvili@gmail.com>
2021-03-13 19:20:23 -0500

    done

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -51,7 +51,7 @@ str.replace(/[^\w]/g, "") //=> "heythere"
 \w    =>  used to find a word character (a character from a-z, A-Z, 0-9, including the underscore)
 [^]   =>  used to find any character NOT between the brackets, in this case, anything that isn't a word character (i.e. spaces & punctuation)
 g     =>  used to perform a global match (find all matches rather than stopping after the first match)
-""    => replace all matches with empty str
+""    =>  replace all matches with empty str
 
 Could've also just used \W:
 .replace(/\W/g, "")
