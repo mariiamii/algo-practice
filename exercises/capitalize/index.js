6bdf3389d38a184ce20d0465ea9f157ccb42c717
Mariam Anuashvili <m.anuashvili@gmail.com>
2021-03-13 22:28:16 -0500

    done

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -6,14 +6,30 @@ capitalize('a lazy fox') --> 'A Lazy Fox'
 capitalize('look, it is working!') --> 'Look, It Is Working!'
 */
 
+// SOLUTION 1:
 function capitalize(str) {
-    let capitalizedArr = []
+    let capArr = []
 
-    for (let word of str.split(" ")) {
-        let capitalizedWord = word[0].toUpperCase() + word.slice(1)
-        capitalizedArr.push(capitalizedWord)
+    str.split(" ").forEach(word => {
+        capArr.push(word[0].toUpperCase() + word.slice(1))
+    })
+
+    return capArr.join(" ")
+}
+
+// SOLUTION 2:
+function capitalize(str) {
+    let capStr = str[0].toUpperCase() //important b/c the below loop could not address the 1st word capitalization
+
+    for (let i = 1; i < str.length; i++) {
+        if (str[i - 1] === " ") { //if there's space to the left of the character
+            capStr += str[i].toUpperCase() //then capitalize it and join it w/ capStr
+        } else {
+            capStr += str[i]
+        }
     }
-    return capitalizedArr.join(" ")
+
+    return capStr
 }
 
 module.exports = capitalize;
