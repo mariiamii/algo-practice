c27f3b0694f761f506bcba705a70252650b1baae
Mariam Anuashvili <m.anuashvili@gmail.com>
2021-03-14 23:47:17 -0400

    working

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -1,8 +1,8 @@
 /* Directions:
-Write a fn that accepts a positive # N. The fn should console.log() a step shape w/ N levels using the # char. Make sure the step has spaces on the right hand side!
+Write a fn that accepts a positive # N. The fn should console.log() a step shape w/ N levels using the # char. Make sure the step has spaces on the right hand side! Note: the # implies the # of lines
 
 steps(2)
-    '# '
+    '# ' //pay attention to the # of spaces after #
     '##'
 steps(3)
     '#  '
@@ -16,7 +16,7 @@ steps(4)
 */
 
 function steps(n) {
-    
+
 }
 
 module.exports = steps;
