abe58548cbd8a24b0ecf2ac9b007d05ed1b3a3d8
Mariam Anuashvili <m.anuashvili@gmail.com>
2021-03-15 23:11:36 -0400

    working

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -15,8 +15,33 @@ steps(4)
     '####'
 */
 
+// SOLUTION 1:
 function steps(n) {
+    
+    for (let i = 0; i < n; i++) { //iterate through rows
+        let stair = ""
 
+        for () { //iterate through columns
+            if () {
+    
+            } else {
+                stair += "#"
+            }
+
+        }
+    }
+    console.log(stair)
 }
 
 module.exports = steps;
+
+/* Explanation:
+1) Iterate through rows (0-n)
+    1) Create an empty str, "stair"
+    2) Iterate through columns (0-n)
+        1) If the current column is equal to or less than the current row
+            Add a "#" to the "stair"
+        2) Else
+            Add a spcase to "stair"
+    3) Console.log("stair")
+*/
