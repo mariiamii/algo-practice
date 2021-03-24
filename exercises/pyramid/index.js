2654d4ecbd737722b9aebe5fe99c842e492b00e7
Mariam Anuashvili <m.anuashvili@gmail.com>
2021-03-24 17:49:54 -0400

    done

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,11 +14,32 @@ pyramid(3)
 
 // SOLUTION 1:
 function pyramid(n) {
-    
+    let midpoint = Math.floor((2 * n - 1) / 2)
+
+    for (let row = 0; row < n; row++) {
+        let level = ""
+
+        for (let column = 0; column < 2 * n - 1; column++) {
+            if (column >= midpoint - row && column <= midpoint + row) {
+                level += "#"
+            } else {
+                level += " "
+            }
+        }
+
+        console.log(level)
+    }
 }
 
 /* Explanation:
-
+1) Iterate through rows (0 - n)
+    1) Create an empty str, "level"
+    2) Iterate through columns (0 - ???)
+        1) If the current column should have a "#"
+            Add a "#" to the "level"
+        2) Else
+            Add a space to "level"
+    3) Console.log("level")
 */
 
 module.exports = pyramid;
