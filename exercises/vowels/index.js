2b17c18310bcca113531cbfb8d9a33264c9ba2fe
Mariam Anuashvili <m.anuashvili@gmail.com>
2021-03-24 19:28:31 -0400

    working

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -1,12 +1,14 @@
-// --- Directions
-// Write a function that returns the number of vowels
-// used in a string.  Vowels are the characters 'a', 'e'
-// 'i', 'o', and 'u'.
-// --- Examples
-//   vowels('Hi There!') --> 3
-//   vowels('Why do you ask?') --> 4
-//   vowels('Why?') --> 0
+/* Directions:
+Write a fn that returns the # of vowels used in a string. Vowels are the characters 'a', 'e', 'i', 'o', and 'u'.
 
-function vowels(str) {}
+vowels('Hi There!') --> 3
+vowels('Why do you ask?') --> 4
+vowels('Why?') --> 0
+*/
+
+// SOLUTION 1:
+function vowels(str) {
+    
+}
 
 module.exports = vowels;
