05287a9aa80a74470a82930efa4f9a3f5112ec20
Mariam Anuashvili <m.anuashvili@gmail.com>
2021-03-25 13:32:26 -0400

    first try done and passing

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -6,9 +6,26 @@ vowels('Why do you ask?') --> 4
 vowels('Why?') --> 0
 */
 
-// SOLUTION 1:
+// SOLUTION 1: iteration
 function vowels(str) {
-    
+    let vowels = ["a", "e", "i", "o", "u"]
+    let arr = []
+
+    for (let char of str.toLowerCase()) {
+        for (let i = 0; i < vowels.length; i++) {
+            if (char === vowels[i]) {
+                arr.push(char)
+            }
+        }
+    }
+
+    return arr.length
 }
+
+/* Explanation
+
+*/
+
+// SOLUTION 2: regular expression
 
 module.exports = vowels;
