dbd0f167d448f6d50001d53b9aef070c0542863d
Mariam Anuashvili <m.anuashvili@gmail.com>
2021-03-25 15:03:34 -0400

    working

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -1,20 +1,26 @@
-// --- Directions
-// Write a function that accepts an integer N
-// and returns a NxN spiral matrix.
-// --- Examples
-//   matrix(2)
-//     [[1, 2],
-//     [4, 3]]
-//   matrix(3)
-//     [[1, 2, 3],
-//     [8, 9, 4],
-//     [7, 6, 5]]
-//  matrix(4)
-//     [[1,   2,  3, 4],
-//     [12, 13, 14, 5],
-//     [11, 16, 15, 6],
-//     [10,  9,  8, 7]]
+/* Directions:
+Write a fn that accepts an integer N & returns a NxN spiral matrix.
 
-function matrix(n) {}
+matrix(2)
+    [[1, 2],
+    [4, 3]]
+matrix(3)
+    [[1, 2, 3],
+    [8, 9, 4],
+    [7, 6, 5]]
+matrix(4)
+    [[1,   2,  3, 4],
+    [12, 13, 14, 5],
+    [11, 16, 15, 6],
+    [10,  9,  8, 7]]
+*/
+
+function matrix(n) {
+    
+}
+
+/* Explanation:
+
+*/
 
 module.exports = matrix;
