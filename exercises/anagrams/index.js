7122abbbde847f8c83b87f4a540c2c7165bd9ad9
Mariam Anuashvili <m.anuashvili@gmail.com>
2021-03-25 14:47:49 -0400

    done

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -36,7 +36,7 @@ function buildCharMap(str) {
     return charMap
 }
 
-// SOLUTION 2:
+// SOLUTION 2: regex
 function anagrams(stringA, stringB) {
     return cleanStr(stringA) === cleanStr(stringB)
 }
@@ -45,16 +45,17 @@ function cleanStr(str) {
     return str.replace(/\W/g, "").toLowerCase().split("").sort().join("")
 }
 
-/* Explanation (on RegEx); sources: https://regexr.com/ & https://www.youtube.com/watch?v=rhzKDrUiJVk:
+/* Explanation (on RegEx); sources: https://regexr.com/ & https://www.w3schools.com/jsref/jsref_obj_regexp.asp & https://www.youtube.com/watch?v=rhzKDrUiJVk:
 str = "hey there!"
 str.replace(/[^\w]/g, "") //=> "heythere" 
 \w    =>  used to find a word character (a character from a-z, A-Z, 0-9, including the underscore)
 [^]   =>  used to find any character NOT between the brackets, in this case, anything that isn't a word character (i.e. spaces & punctuation)
-g     =>  used to perform a global match (find all matches rather than stopping after the first match)
+g     =>  does a global match (find all matches rather than stopping after the 1st match)
 ""    =>  replace all matches with empty str
 
 Could've also just used \W:
 .replace(/\W/g, "")
+\W    =>  used to do a global search for non-word characters in a str
 */
 
 module.exports = anagrams;
